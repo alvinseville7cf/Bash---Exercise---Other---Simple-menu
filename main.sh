32a8c39be3b8218b7eb3f123c35f17ce0cb16e40
Alvin Seville <alvinseville7cf@gmail.com>
2021-02-12 10:14:29 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env bash
+
+# Call syntax: getMaxLength <items>
+function getMaxLength()
+{
+  local value=$1
+  local -i max=${#value}
+  shift
+
+  while [[ -n $1 ]]
+  do
+    value=$1
+    (( "${#value}" > max )) && max=${#value}
+    shift
+  done
+
+  echo $max
+}
+
+# Call syntax: drawMenu --selected|-s <index> --foreground|-f <foreground> --background|-b <background> -- <items>
+function drawMenu()
+{
+  local -ir UnexpectedEndOfArgumentList=1
+  local -ir UnrecognizedOptionInArgumentList=2
+  local -ir SelectedLessThanZero=3
+  local -ir SelectedExceedsItemCount=4
+
+  local -ir DefaultForeground=37
+  local -ir DefaultBackground=40
+
+  while [[ -n $1 ]]
+  do
+    local option=$1
+    local value=$2
+
+    case $option in
+      --selected|-s)
+          local -i selected=$value
+          shift 2 || return $UnexpectedEndOfArgumentList
+        ;;
+      --foreground|-f)
+          local -i foreground=$value
+          shift 2 || return $UnexpectedEndOfArgumentList
+        ;;
+      --background|-b)
+          local -i background=$value
+          shift 2 || return $UnexpectedEndOfArgumentList
+        ;;
+      --)
+          shift
+          break
+        ;;
+      *)
+          return $UnrecognizedOptionInArgumentList
+        ;;
+    esac
+  done
+
+  local values=()
+  local value=$1
+  while [[ -n $1 ]]
+  do
+    values+=($1)
+    shift
+  done
+
+  (( selected < 0 )) && return $SelectedLessThanZero
+  (( selected > "${#values[@]}" - 1 )) && return $SelectedExceedsItemCount
+
+  local -i max=$(getMaxLength "${values[@]}")
+
+  local -i i=0
+  for value in "${values[@]}"
+  do
+    local placeholder="$(printf " %0.0s" $(seq 0 $(($max - ${#value}))))"
+    (( selected == i )) && underline='4;' || underline=''
+    echo -e "\e[${underline}${foreground};${background}m$value${placeholder:1}\e[0m"
+    ((i++))
+  done
+}
+
+# Call syntax: selectMainMenu
+function selectMainMenu()
+{
+  local items=(Add Replace Remove)
+  local -i foreground=37
+  local -i background=44
+  local -i selected=0
+
+  drawMenu --selected $selected --foreground $foreground --background $background -- "${items[@]}"
+
+  read -sr -n 1 char
+  while [[ $char != 'e' ]]
+  do
+    case $char in
+      'w')
+          ((selected--))
+          (( selected < 0 )) && selected=0
+        ;;
+      's')
+          ((selected++))
+          (( selected > "${#items[@]}" - 1 )) && selected="${#items[@]} - 1"
+        ;;
+      *)
+        ;;
+    esac
+    echo -ne "\e[3A"
+    drawMenu --selected $selected --foreground $foreground --background $background -- "${items[@]}"
+    read -sr -n 1 char
+  done
+}
+
+tput civis
+selectMainMenu
